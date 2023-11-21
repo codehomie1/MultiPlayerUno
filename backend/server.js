147b2cf4ad8b5de2211259f95b39e56550161de1
Mohammed <mohammedhmohamed02@gmail.com>
2023-11-21 13:03:14 -0800

    morgan and cookies

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const createError = require("http-errors");
 const path = require("path");
 
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -18,6 +20,12 @@ if (process.env.NODE_ENV === "development") {
   });
   app.use(connectLiveReload());
 }
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser);
 
 app.set("views", path.join(__dirname, "views")); 
 app.set("view engine", "ejs");
