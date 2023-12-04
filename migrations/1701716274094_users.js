c8bccd8d4ed940a793eeabe7046df72f2abd54b5
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-04 11:03:33 -0800

    users table setup

---
+++ migrations/1701716274094_users.js
@@ -0,0 +1,27 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: "id",
+    email: {
+      type: "varchar(256)",
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: "char(60)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("users");
+};
