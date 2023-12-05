4b398a4017a7b30895c07c3fdc97684f3dcd25b7
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-04 16:19:57 -0800

    setup user queries

---
+++ backend/db/users.js
@@ -0,0 +1,27 @@
+const db = require("./connections");
+
+const USERS_EXISTENCE = `SELECT COUNT(*) 
+FROM users 
+WHERE email=$1 `;
+
+const ADD_USER = `INSERT INTO users (email, password) 
+VALUES ($1, $2) 
+RETURNING id, email`;
+
+const SIGN_USER_IN = `SELECT * FROM users 
+WHERE email=$1`;
+
+const email_exists = (email) =>
+  db
+    .one(USERS_EXISTENCE, [email])
+    .then((_) => true)
+    .catch((_) => false);
+
+const create = () => db.one(ADD_USER, [email, password]);
+const find_by_email = (email) => db.one(SIGN_USER_IN, [email]);
+
+module.exports = {
+  email_exists,
+  create,
+  find_by_email,
+};
