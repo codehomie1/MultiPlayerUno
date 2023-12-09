13f311d6ee307c942b65ec4274aeeeaece0e82b7
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-09 15:28:25 -0800

    update lobby route

--- backend/routes/global_lobby.js
+++ backend/routes/global_lobby.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("lobby");
+  if (req.username) {
+    res.render("lobby", { name: req.username });
+  }
+  res.render("lobby", { name: "Guest" });
+});
+
+router.get("/:name", (req, res) => {
+  res.render("lobby", { name: req.params.name });
 });
 
 module.exports = router;
