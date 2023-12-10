98d38290f42bc356efe83595b50c9923a93a4835
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-09 17:59:25 -0800

    update lobby route

--- backend/routes/global_lobby.js
+++ backend/routes/global_lobby.js
@@ -2,13 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  if (req.username) {
-    res.render("lobby", { name: req.username });
+  console.log("--------in lobby route ---------");
+  console.log(req.session);
+  console.log("--------in lobby route ---------");
+
+  if (req.session.username) {
+    res.render("lobby", { name: req.session.username });
   }
+
   res.render("lobby", { name: "Guest" });
 });
 
 router.get("/:name", (req, res) => {
+  console.log("--------in lobby route 2---------");
+  console.log(req.session);
+  console.log("--------in lobby route2 ---------");
   res.render("lobby", { name: req.params.name });
 });
 
