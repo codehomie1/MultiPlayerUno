b184bedf7645df891e2a0daa8fff4a95a1643d4e
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-13 19:02:07 -0800

    update lobby route

--- backend/routes/lobby.js
+++ backend/routes/lobby.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// TODO: GET SESSION saved in redirect
-
 router.get("/", (req, res) => {
   console.log("--------in lobby route ---------");
   console.log(req.session);
   console.log("--------in lobby route ---------");
 
-  if (req.session.username) {
+  if (req.session.user.username) {
     res.render("lobby", { name: req.session.user.username });
   }
 
