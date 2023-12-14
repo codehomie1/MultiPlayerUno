2431e324cc09c7cb0bfc3bdb48fa56228b7933de
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-13 20:14:09 -0800

    update server

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,11 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const PORT = process.env.PORT || 3000;
 
-const { viewSessionData } = require("./middleware/view-session.js");
-const { sessionLocals } = require("./middleware/session-locals.js");
-const { isAuthenticated } = require("./middleware/is-authenticated.js");
+const {
+  viewSessionData,
+  sessionLocals,
+  isAuthenticated,
+} = require("./middleware/");
 
 const express = require("express");
 const app = express();
@@ -59,6 +61,9 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(
   session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
@@ -72,21 +77,12 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(sessionLocals);
 
-const landingRoutes = require("./routes/landing");
-const authRoutes = require("./routes/authentication");
-const lobbyRoutes = require("./routes/lobby");
-const gameRoutes = require("./routes/game");
-const testRoutes = require("./routes/test/index.js");
-const { log } = require("console");
+const Routes = require("./routes");
 
-app.use(["/landing", "/"], landingRoutes);
-app.use("/auth", authRoutes);
-
-// must be authenticated to get to lobby and games route
-app.use("/lobby", isAuthenticated, lobbyRoutes);
-app.use("/games", isAuthenticated, gameRoutes);
-
-app.use("/db", testRoutes);
+app.use(["/landing", "/"], Routes.landing);
+app.use("/auth", Routes.authentication);
+app.use("/lobby", isAuthenticated, Routes.lobby);
+app.use("/games", isAuthenticated, Routes.game);
 
 app.use((_req, _res, next) => {
   next(createError(404));
