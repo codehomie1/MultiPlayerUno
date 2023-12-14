00d8b2e75383ae631da50037728eaa8115d4d846
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-13 19:03:04 -0800

    update server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const createError = require("http-errors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const { Client } = require("pg");
+const bodyParser = require("body-parser");
 const session = require("express-session");
 const PORT = process.env.PORT || 3000;
 
@@ -16,21 +17,40 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "static")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+console.log("--------process NODE_ENV---------");
+console.log(process.env.NODE_ENV);
+console.log("--------process NODE_ENV---------");
+
+console.log("--------session secret---------");
+console.log(process.env.SESSION_SECRET);
+console.log("--------session secret---------");
+
+console.log("------process === development");
+console.log(process.env.NODE_ENV === "development");
+
 if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+
   const livereload = require("livereload");
   const connectLiveReload = require("connect-livereload");
-  const liveReloadServer = livereload.createServer();
 
-  liveReloadServer.watch(path.join(__dirname, "backend", "static"));
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "static"));
   liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
-      liveReloadServer.refresh("/landing");
+      liveReloadServer.refresh("/");
     }, 100);
   });
 
@@ -39,17 +59,14 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(
   session({
-    store: new (require("connect-pg-simple")(session))({
-      createTableIfMissing: true,
-    }),
     secret: process.env.SESSION_SECRET,
-    saveUninitialized: true,
     resave: false,
-    cookie: { secure: process.env.NODE_ENV != "development" },
+    saveUninitialized: true,
+    cookie: { secure: process.env.NODE_ENV !== "development" },
   }),
 );
 
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === "development") {
   app.use(viewSessionData);
 }
 
@@ -60,6 +77,7 @@ const authRoutes = require("./routes/authentication");
 const lobbyRoutes = require("./routes/lobby");
 const gameRoutes = require("./routes/game");
 const testRoutes = require("./routes/test/index.js");
+const { log } = require("console");
 
 app.use(["/landing", "/"], landingRoutes);
 app.use("/auth", authRoutes);
