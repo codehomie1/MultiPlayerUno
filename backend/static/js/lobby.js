a590384eab444c3ee6dd217dd8f589d150483aec
Mohammed <mohammedhmohamed02@gmail.com>
2023-12-15 21:18:48 -0800

    waiting room when click game

--- backend/static/js/lobby.js
+++ backend/static/js/lobby.js
@@ -21,64 +21,63 @@ const getUserSession = async () => {
 };
 
 const getAllMessages = async () => {
-    try {
-      const res = await fetch("/lobby/get-messages", { method: "GET" });
-      const data = await res.json();
-      const messageArray = data.messageArray;
-  
-      if (data.status === 400 || data.status === 500) {
-        return;
-      }
-  
-      let chatList = document.getElementById("chatMessages");
-  
-      if (!chatList) {
-        return;
-      }
-  
-      chatList.innerHTML = "";
-  
-      messageArray.map((msg) => {
-        const date = new Date(msg.created_at);
-  
-        const createdAtFormatted = date.toLocaleString("en-US", {
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-        });
-  
-        let li = document.createElement("div");
-        li.className = "message";
-  
-        let usernameSpan = document.createElement("span");
-        usernameSpan.style.fontWeight = "bold";
-        usernameSpan.textContent = msg.username;
-  
-        let messageP = document.createElement("p");
-        messageP.style.margin = "5px";
-        messageP.textContent = msg.message;
-        
-        messageP.classList.add("chat-message");
-        messageP.classList.add("user");
-        li.appendChild(usernameSpan);
-        // li.appendChild(createdAtSpan);
-        // li.appendChild(document.createTextNode(": "));
-        li.appendChild(messageP);
-  
-        chatList.appendChild(li);
-      });
-    } catch (err) {
-      console.log(err);
+  try {
+    const res = await fetch("/lobby/get-messages", { method: "GET" });
+    const data = await res.json();
+    const messageArray = data.messageArray;
+
+    if (data.status === 400 || data.status === 500) {
+      return;
     }
+
+    let chatList = document.getElementById("chatMessages");
+
+    if (!chatList) {
+      return;
+    }
+
+    chatList.innerHTML = "";
+
+    messageArray.map((msg) => {
+      const date = new Date(msg.created_at);
+
+      const createdAtFormatted = date.toLocaleString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      });
+
+      let li = document.createElement("div");
+      li.className = "message";
+
+      let usernameSpan = document.createElement("span");
+      usernameSpan.style.fontWeight = "bold";
+      usernameSpan.textContent = msg.username;
+
+      let messageP = document.createElement("p");
+      messageP.style.margin = "5px";
+      messageP.textContent = msg.message;
+
+      messageP.classList.add("chat-message");
+      messageP.classList.add("user");
+      li.appendChild(usernameSpan);
+      // li.appendChild(createdAtSpan);
+      // li.appendChild(document.createTextNode(": "));
+      li.appendChild(messageP);
+
+      chatList.appendChild(li);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const sendMessage = async () => {
-    const form = document.getElementById("send-chat-form-id");
-    const formData = new FormData(form);
-    const formDataJson = {};
-    
+  const form = document.getElementById("send-chat-form-id");
+  const formData = new FormData(form);
+  const formDataJson = {};
 
   for (const [key, value] of formData) {
     formDataJson[key] = value;
@@ -90,90 +89,93 @@ const sendMessage = async () => {
   // const game_id = getGameId(document.location.pathname);
   // formDataJson["game_id"] = game_id;
 
-    const options = {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formDataJson),
-    };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formDataJson),
+  };
 
-    // console.log("---------messageInfo---------");
-    // console.log(options.body);
+  // console.log("---------messageInfo---------");
+  // console.log(options.body);
 
   try {
     const res = await fetch("/lobby/send-message", options);
     const data = await res.json();
     getAllMessages();
 
-        if (data.status === 400 || data.status === 500) {
-            showMessage(data);
-            getAllMessages();
-            return;
-        }
-        } catch (err) {
-            console.log(err);
-        }
+    if (data.status === 400 || data.status === 500) {
+      showMessage(data);
+      getAllMessages();
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const createGame = async () => {
-    const form = document.getElementById("create-game-form-id");
-    const formData = new FormData(form);
-    const formDataJson = {};
-  
-    for (const [key, value] of formData) {
-      formDataJson[key] = value;
-    }
-  
-    const userSession = await getUserSession();
-    formDataJson["user_id"] = userSession.id;
-  
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formDataJson),
-    };
-  
-    try {
-      const res = await fetch("/lobby/create-game", options);
-      const data = await res.json();
-      getGames();
-      if (data.status === 400) {
-        showMessage(data);
-        return;
-      }
-    } catch (err) {
-      console.log(err);
-    }
+  const form = document.getElementById("create-game-form-id");
+  const formData = new FormData(form);
+  const formDataJson = {};
+
+  for (const [key, value] of formData) {
+    formDataJson[key] = value;
+  }
+
+  const userSession = await getUserSession();
+  formDataJson["user_id"] = userSession.id;
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formDataJson),
   };
-  
-  const getGames = async() => {
-    try {
-        const res = await fetch("/lobby/get-games", { method: "GET" });
-        const data = await res.json();
-        const messageArray = data.messageArray;
-    
-        if (data.status === 400 || data.status === 500) {
-          return;
-        }
-    
-        let gameList = document.getElementById("games-list-id");
-    
-        if (!gameList) {
-          return;
-        }
-    
-        gameList.innerHTML = "";
-    
-        messageArray.map((msg) => {
-          let li = document.createElement("div");
-        //   li.className = "game";
-          li.innerHTML = `<a class="game-room-link" href="#">Title: ${msg.game_title}, # ${msg.id}, Players: ${msg.users_required}, Started: ${msg.ongoing}</a>`;
-          gameList.appendChild(li);
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    };
+
+  try {
+    const res = await fetch("/lobby/create-game", options);
+    const data = await res.json();
+    getGames();
+    if (data.status === 400) {
+      showMessage(data);
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getGames = async () => {
+  try {
+    const res = await fetch("/lobby/get-games", { method: "GET" });
+    const data = await res.json();
+    const messageArray = data.messageArray;
+
+    if (data.status === 400 || data.status === 500) {
+      return;
+    }
+
+    let gameList = document.getElementById("games-list-id");
+
+    if (!gameList) {
+      return;
+    }
+
+    gameList.innerHTML = "";
+
+    messageArray.map((msg) => {
+      let li = document.createElement("div");
+      //   li.className = "game";
+      li.innerHTML = `<a class="game-room-link" href="/waitingroom">Title: ${msg.game_title}, # ${msg.id}, Players: ${msg.users_required}, Started: ${msg.ongoing}</a>`;
+      gameList.appendChild(li);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getAllMessages();
+getGames();
